bb3c1504d175ea47fc0eb4ad20ee3e373b511611
rakesh.ramesan <rakesh.ramesan@savealot.com>
2022-06-29 10:08:02 -0500

    local changes

--- src/main/java/com/myapp/plcconnect/controller/MainController.java
+++ src/main/java/com/myapp/plcconnect/controller/MainController.java
@@ -8,7 +8,6 @@ import com.myapp.plcconnect.repo.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
