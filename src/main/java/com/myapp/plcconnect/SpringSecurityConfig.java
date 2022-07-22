be89442e999561f74296155c8406a69f1cc2f32b
Rakesh Ramesan <ramesan.rakesh@gmail.com>
2022-07-21 22:40:02 -0500

    message updates

--- src/main/java/com/myapp/plcconnect/SpringSecurityConfig.java
+++ src/main/java/com/myapp/plcconnect/SpringSecurityConfig.java
@@ -1,22 +1,19 @@
 package com.myapp.plcconnect;
 
-import com.myapp.plcconnect.filters.JwtRequestFilter;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.myapp.plcconnect.filters.JwtRequestFilter;
 
 @EnableWebSecurity @Configuration
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
@@ -36,7 +33,9 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
-            .authorizeRequests().antMatchers("/authenticate").permitAll()
+            .authorizeRequests()
+            .antMatchers("/authenticate").permitAll()
+            .antMatchers("/postmessage").permitAll()
             .anyRequest().authenticated()
             .and().sessionManagement()
             .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
--- src/main/java/com/myapp/plcconnect/model/Message.java
+++ src/main/java/com/myapp/plcconnect/model/Message.java
@@ -1,5 +1,7 @@
 package com.myapp.plcconnect.model;
 
+import java.util.Date;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -10,49 +12,111 @@ public class Message {
    @Id
    @GeneratedValue(strategy=GenerationType.AUTO)
     private long id;
+    public long getId() {
+    return id;
+}
+public void setId(long id) {
+    this.id = id;
+}
+public long getClientId() {
+    return clientId;
+}
+public void setClientId(long clientId) {
+    this.clientId = clientId;
+}
+public Integer getCycleCount() {
+    return cycleCount;
+}
+public void setCycleCount(Integer cycleCount) {
+    this.cycleCount = cycleCount;
+}
+public Integer getCoilCount() {
+    return coilCount;
+}
+public void setCoilCount(Integer coilCount) {
+    this.coilCount = coilCount;
+}
+public Integer getCycleTime() {
+    return cycleTime;
+}
+public void setCycleTime(Integer cycleTime) {
+    this.cycleTime = cycleTime;
+}
+public String getMachineId() {
+    return machineId;
+}
+public void setMachineId(String machineId) {
+    this.machineId = machineId;
+}
+public String getJobName() {
+    return jobName;
+}
+public void setJobName(String jobName) {
+    this.jobName = jobName;
+}
+public Integer getJobNo() {
+    return jobNo;
+}
+public void setJobNo(Integer jobNo) {
+    this.jobNo = jobNo;
+}
+public Integer getTotalTurns() {
+    return totalTurns;
+}
+public void setTotalTurns(Integer totalTurns) {
+    this.totalTurns = totalTurns;
+}
+public Integer getCurrentTurns() {
+    return CurrentTurns;
+}
+public void setCurrentTurns(Integer currentTurns) {
+    CurrentTurns = currentTurns;
+}
+public Integer getLayerTurns() {
+    return layerTurns;
+}
+public void setLayerTurns(Integer layerTurns) {
+    this.layerTurns = layerTurns;
+}
+public Integer getQuantity() {
+    return quantity;
+}
+public void setQuantity(Integer quantity) {
+    this.quantity = quantity;
+}
+public Integer getWindSpeed() {
+    return windSpeed;
+}
+public void setWindSpeed(Integer windSpeed) {
+    this.windSpeed = windSpeed;
+}
+public Integer getAccelerationSpeed() {
+    return accelerationSpeed;
+}
+public void setAccelerationSpeed(Integer accelerationSpeed) {
+    this.accelerationSpeed = accelerationSpeed;
+}
+public Date getDateTime() {
+    return dateTime;
+}
+public void setDateTime(Date dateTime) {
+    this.dateTime = dateTime;
+}
     private long clientId;
     private Integer cycleCount;
     private Integer coilCount;
     private Integer cycleTime;
     private String machineId;
-
+    private String jobName;
+    private Integer  jobNo;
+    private Integer totalTurns;
+    private Integer CurrentTurns;
+    private Integer layerTurns;
+    private Integer quantity;
+    private Integer windSpeed;
+    private Integer accelerationSpeed;
+    private Date dateTime;    
     
-    public long getId() {
-        return id;
-    }
-    public void setId(long id) {
-        this.id = id;
-    }
-    public long getClientId() {
-        return clientId;
-    }
-    public void setClientId(long clientId) {
-        this.clientId = clientId;
-    }
-    public Integer getCycleCount() {
-        return cycleCount;
-    }
-    public void setCycleCount(Integer cycleCount) {
-        this.cycleCount = cycleCount;
-    }
-    public Integer getCoilCount() {
-        return coilCount;
-    }
-    public void setCoilCount(Integer coilCount) {
-        this.coilCount = coilCount;
-    }
-    public Integer getCycleTime() {
-        return cycleTime;
-    }
-    public void setCycleTime(Integer cycleTime) {
-        this.cycleTime = cycleTime;
-    }
-    public void setMachineId(String machineId){
-    	this.machineId=machineId;
-    }
-    public String getMachineId(){
-    	return this.machineId;
-    }
 
     
 }
